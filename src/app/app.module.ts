691670e01db1a87688e26dbf35c19a8a6a48ec92
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-03-03 13:08:31 -0800

    Initial commit - Mod5 - assignment 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { ContactComponent } from './contact/contact.component';
+
+const ROUTES: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
